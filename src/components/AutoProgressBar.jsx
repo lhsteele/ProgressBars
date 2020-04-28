7ab2a10defd5c765499a542b2b02293ac804ed16
Lisa Steele <lisahsteele@gmail.com>
2020-04-28 13:47:23 -0700

    Add logic to increment the progress bar automatically on button click.

--- src/components/AutoProgressBar.jsx
+++ src/components/AutoProgressBar.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 
 export default class AutoProgressBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initialFill: 50
+    };
+  };
+
+  incrementBar = () => {
+    let currentFill = this.state.initialFill
+    currentFill += 50;
+    this.setState({ initialFill: currentFill })
+  };
+
+  autoLoad = () => {
+    setInterval(this.incrementBar.bind(this), 1000);
+  };
+
   render() {
     return (
       <div className="outer-container">
         <h1 className="auto-load-label">Auto Loading Progress Bar</h1>
-        <div className="auto-progress-bar"/>
-        <button className="auto-load-btn">Load</button>
+        <div className="auto-progress-bar">
+          <div className="progress-bar" style={{ width: this.state.initialFill }}/>
+        </div>
+        <button 
+          className="auto-load-btn"
+          onClick={this.autoLoad}>
+          Load
+        </button>
       </div>
     )
   }
